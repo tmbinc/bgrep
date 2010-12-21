5ae68d2c759c14cfeeead6c7cf68e2fbdbe8accf
Felix Domke <felix.domke@multimedia-labs.de>
2010-12-21 11:45:05 +0100

    add another test using random data

---
+++ test/bgrep-random.py
@@ -0,0 +1,35 @@
+import os, subprocess, random
+
+BGREP="../bgrep"
+
+def generate_random(datalen, searchlen):
+	data = os.urandom(datalen)
+	search = os.urandom(searchlen)
+	
+	results = []
+	for i in range(datalen-searchlen+1):
+		if data[i:i+searchlen] == search:
+			results.append(i)
+	return data, search, results
+
+
+def test_bgrep(datalen, searchlen):
+	data, search, results = generate_random(datalen, searchlen)
+	filename = "data"
+	open(filename, "wb").write(data)
+	
+	bgrep_res = subprocess.Popen([BGREP, search.encode('hex'), filename], stdout=subprocess.PIPE).communicate()[0]
+	
+	expected_res = ''.join(["%s: %08x\n" % (filename, i) for i in results])
+	
+	if bgrep_res != expected_res:
+		print "search: %s" % search.encode('hex')
+		open("res_expected", "w").write(expected_res)
+		open("res_bgrep", "w").write(bgrep_res)
+		assert False
+
+while True:
+	datalen = random.randint(0, 1024*1024)
+	searchlen = random.randint(1, 50)
+#	print datalen, searchlen
+	test_bgrep(datalen, searchlen)
