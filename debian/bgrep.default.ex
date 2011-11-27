7495bba5389a6785c45a4aca7315dd4befc2e164
Philippe Coval <rzr@gna.org>
2011-11-27 22:57:22 +0100

    Add: debianize

---
+++ debian/bgrep.default.ex
@@ -0,0 +1,10 @@
+# Defaults for bgrep initscript
+# sourced by /etc/init.d/bgrep
+# installed at /etc/default/bgrep by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
