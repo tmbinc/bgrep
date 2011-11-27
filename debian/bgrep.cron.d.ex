562d65a241539ad412d719ffa4f05413d7affcca
Philippe Coval <rzr@gna.org>
2011-11-27 22:59:09 +0100

    Imported Upstream version 0.0.0

--- debian/bgrep.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the bgrep package
-#
-0 4	* * *	root	[ -x /usr/bin/bgrep_maintenance ] && /usr/bin/bgrep_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(bgrep):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="bgrep" command="/usr/bin/bgrep"
