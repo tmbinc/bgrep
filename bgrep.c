tmbinc/bgrep

commit fc2a6cbc0f34dd01d5bd4a3c1ead0324cccdcc5a

Support External Pattern & Mask Files (#21)
Adds support for external pattern and mask files with the `-f` and `-m`
arguments, respectively. Both files are expected to be binary files
containing the bits to be matched. Files are read in byte order (any
endianness must be taken care of by the creator). At the moment, `-m` is
only available with `-f`, but that is not a strict limitation.

Increases the default buffer size to 1MB to increase throughput. For the
tested use case this reduced processing time by half.

Adds an `-r` option to enable recursion to match POSIX grep.

Fixes an off-by-one error when allocating space for the next path when
recursing.

Adds additional tests for using a pattern file as well as a pattern file
with a mask. Test failures now terminate early with a non-zero exit
code. `bgrep-test.sh` can now be run from any directory (such as the
project root).

bgrep.c
@@ -25,6 +25,9 @@
 // or implied, of the copyright holder.
 //
 
+#include <err.h>
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -136,10 +139,11 @@ void searchfile(const char *filename, int fd, const unsigned char *value, const 
 	off_t offset = 0;
 
 	// use a search buffer which is at least the next power of two after len
-	size_t bufsize = 1024;
-	while (bufsize <= (size_t)len)
+	// and on a 16-byte boundary
+	size_t bufsize = 1024 * 1024;
+	while (bufsize <= (size_t)len || (bufsize & 0xF) != 0)
 		bufsize <<= 1;
-	unsigned char *buf = malloc(bufsize);
+	unsigned char *buf = calloc(bufsize, 1);
 
 	if (!buf)
 	{
@@ -168,6 +172,7 @@ void searchfile(const char *filename, int fd, const unsigned char *value, const 
 			for (i = 0; i <= len; ++i)
 				if ((buf[o + i] & mask[i]) != value[i])
 					break;
+
 			if (i > len)
 			{
 				unsigned long long pos = (unsigned long long)(offset + o - len);
@@ -177,20 +182,29 @@ void searchfile(const char *filename, int fd, const unsigned char *value, const 
 			}
 		}
 
-		offset += r;
-
+		offset += o;
 	}
 
 	free(buf);
 }
 
-void recurse(const char *path, const unsigned char *value, const unsigned char *mask, int len)
+typedef struct Options {
+	char* pattern_path;
+	char* mask_path;
+	bool recurse;
+} Options;
+
+void recurse(const char *path, const unsigned char *value, const unsigned char *mask, int len, const Options* options)
 {
 	struct stat s;
 	if (stat(path, &s))
 	{
 		perror("stat");
 		return;
+	}
+	if (!options->recurse && S_ISDIR(s.st_mode)) {
+		errno = EISDIR;
+		err(1, "%s", path);
 	}
 	if (!S_ISDIR(s.st_mode))
 	{
@@ -217,11 +231,11 @@ void recurse(const char *path, const unsigned char *value, const unsigned char *
 	{
 		if (!(strcmp(d->d_name, ".") && strcmp(d->d_name, "..")))
 			continue;
-		char newpath[strlen(path) + strlen(d->d_name) + 1];
+		char newpath[strlen(path) + 1 + strlen(d->d_name) + 1];
 		strcpy(newpath, path);
 		strcat(newpath, "/");
 		strcat(newpath, d->d_name);
-		recurse(newpath, value, mask, len);
+		recurse(newpath, value, mask, len, options);
 	}
 
 	closedir(dir);
@@ -240,15 +254,18 @@ void die(const char* msg, ...)
 void usage(char** argv)
 {
 	fprintf(stderr, "bgrep version: %s\n", BGREP_VERSION);
-	fprintf(stderr, "usage: %s [-B bytes] [-A bytes] [-C bytes] <hex> [<path> [...]]\n", *argv);
+	fprintf(stderr, "usage:\n");
+	fprintf(stderr, "	%s [-r] [-B bytes] [-A bytes] [-C bytes] <hex> [<path> [...]]\n", *argv);
+	fprintf(stderr, "	%s [-r] -f <pattern> [-m <mask>] [<path> [...]]\n", *argv);
 	exit(1);
 }
 
-void parse_opts(int argc, char** argv)
+void parse_opts(int argc, char** argv, Options* options)
 {
 	int c;
+	char* pattern_path, * mask_path;
 
-	while ((c = getopt(argc, argv, "A:B:C:")) != -1)
+	while ((c = getopt(argc, argv, "A:B:C:f:m:r")) != -1)
 	{
 		switch (c)
 		{
@@ -260,6 +277,15 @@ void parse_opts(int argc, char** argv)
 				break;
 			case 'C':
 				bytes_before = bytes_after = atoi(optarg);
+				break;
+			case 'f':
+				options->pattern_path = optarg;
+				break;
+			case 'm':
+				options->mask_path = optarg;
+				break;
+			case 'r':
+				options->recurse = true;
 				break;
 			default:
 				usage(argv);
@@ -274,6 +300,7 @@ void parse_opts(int argc, char** argv)
 
 int main(int argc, char **argv)
 {
+	Options options = { 0 };
 	unsigned char *value, *mask;
 	int len = 0;
 
@@ -283,105 +310,179 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	parse_opts(argc, argv);
-	argv += optind - 1; /* advance the pointer to the first non-opt arg */
-	argc -= optind - 1;
+	parse_opts(argc, argv, &options);
+	argv += optind; /* advance the pointer to the first non-opt arg */
+	argc -= optind;
 
-	char *h = argv[1];
-	enum {MODE_HEX,MODE_TXT,MODE_TXT_ESC} parse_mode = MODE_HEX;
+	if (options.pattern_path) {
+		struct stat st;
+		if (stat(options.pattern_path, &st)) {
+			perror(options.pattern_path);
+			exit(3);
+		}
+		size_t maxlen = st.st_size;
+		value = malloc(maxlen);
+		mask = malloc(maxlen);
 
-	// Limit the search string dynamically based on the input string.
-	// The contents of value/mask may end up much shorter than argv[1],
-	// but should never be longer.
-	size_t maxlen = strlen(h);
-	value = malloc(maxlen);
-	mask  = malloc(maxlen);
-
-	if (!value || !mask)
-	{
-		die("error allocating memory for search string!\n");
-	}
-
-	while (*h && (parse_mode != MODE_HEX || h[1]) && len < maxlen)
-	{
-		int on_quote = (h[0] == '"');
-		int on_esc = (h[0] == '\\');
-
-		switch (parse_mode)
+		if (!value || !mask)
 		{
-			case MODE_HEX:
-				if (on_quote)
+			die("error allocating memory for search string!\n");
+		}
+
+		FILE* f = fopen(options.pattern_path, "r");
+		if (!f) {
+			perror(options.pattern_path);
+			exit(3);
+		}
+
+		size_t read = 0;
+		while (read < maxlen) {
+			int n = fread(value + read, 1, maxlen, f);
+			if (feof(f) || ferror(f)) {
+				perror(options.pattern_path);
+				exit(3);
+			}
+			read += n;
+		}
+		fclose(f);
+		len = maxlen;
+
+		if (!options.mask_path) {
+			memset(mask, 0xFF, len);
+		} else {
+			if (stat(options.mask_path, &st))
+			{
+				perror(options.mask_path);
+				exit(3);
+			}
+
+			if (st.st_size != len)
+			{
+				fprintf(stderr,
+						"Mask (%zu bytes) must be the same size as pattern (%u bytes)\n",
+						st.st_size,
+						len);
+				exit(3);
+			}
+
+			f = fopen(options.mask_path, "r");
+			read = 0;
+
+			while (read < maxlen)
+			{
+				int n = fread(mask + read, 1, maxlen, f);
+				if (feof(f) || ferror(f))
 				{
+					perror(options.pattern_path);
+					exit(3);
+				}
+				read += n;
+			}
+			fclose(f);
+
+			// pre-mask the pattern
+			for (int i = 0; i < len; i++)
+				value[i] &= mask[i];
+		}
+	}
+	else
+	{
+		char *h = *argv++;
+		argc--;
+		enum {MODE_HEX,MODE_TXT,MODE_TXT_ESC} parse_mode = MODE_HEX;
+
+		// Limit the search string dynamically based on the input string.
+		// The contents of value/mask may end up much shorter than argv[1],
+		// but should never be longer.
+		size_t maxlen = strlen(h);
+		value = malloc(maxlen);
+		mask  = malloc(maxlen);
+
+		if (!value || !mask)
+		{
+			die("error allocating memory for search string!\n");
+		}
+
+		while (*h && (parse_mode != MODE_HEX || h[1]) && len < maxlen)
+		{
+			int on_quote = (h[0] == '"');
+			int on_esc = (h[0] == '\\');
+
+			switch (parse_mode)
+			{
+				case MODE_HEX:
+					if (on_quote)
+					{
+						parse_mode = MODE_TXT;
+						h++;
+						continue; /* works under switch - will continue the loop*/
+					}
+					break; /* this one is for switch */
+				case MODE_TXT:
+					if (on_quote)
+					{
+						parse_mode = MODE_HEX;
+						h++;
+						continue;
+					}
+
+					if (on_esc)
+					{
+						parse_mode = MODE_TXT_ESC;
+						h++;
+						continue;
+					}
+
+					value[len] = h[0];
+					mask[len++] = 0xff;
+					h++;
+					continue;
+
+				case MODE_TXT_ESC:
+					value[len] = h[0];
+					mask[len++] = 0xff;
 					parse_mode = MODE_TXT;
 					h++;
-					continue; /* works under switch - will continue the loop*/
-				}
-				break; /* this one is for switch */
-			case MODE_TXT:
-				if (on_quote)
-				{
-					parse_mode = MODE_HEX;
-					h++;
 					continue;
-				}
-
-				if (on_esc)
-				{
-					parse_mode = MODE_TXT_ESC;
-					h++;
-					continue;
-				}
-
-				value[len] = h[0];
-				mask[len++] = 0xff;
-				h++;
-				continue;
-
-			case MODE_TXT_ESC:
-				value[len] = h[0];
-				mask[len++] = 0xff;
-				parse_mode = MODE_TXT;
-				h++;
-				continue;
-		}
-		//
-		if (h[0] == '?' && h[1] == '?')
-		{
-			value[len] = mask[len] = 0;
-			len++;
-			h += 2;
-		} else if (h[0] == ' ')
-		{
-			h++;
-		} else
-		{
-			int v0 = ascii2hex(*h++);
-			int v1 = ascii2hex(*h++);
-
-			if ((v0 == -1) || (v1 == -1))
-			{
-				fprintf(stderr, "invalid hex string!\n");
-				free(value); free(mask);
-				return 2;
 			}
-			value[len] = (v0 << 4) | v1; mask[len++] = 0xFF;
+			//
+			if (h[0] == '?' && h[1] == '?')
+			{
+				value[len] = mask[len] = 0;
+				len++;
+				h += 2;
+			} else if (h[0] == ' ')
+			{
+				h++;
+			} else
+			{
+				int v0 = ascii2hex(*h++);
+				int v1 = ascii2hex(*h++);
+
+				if ((v0 == -1) || (v1 == -1))
+				{
+					fprintf(stderr, "invalid hex string!\n");
+					free(value); free(mask);
+					return 2;
+				}
+				value[len] = (v0 << 4) | v1; mask[len++] = 0xFF;
+			}
+		}
+
+		if (!len || *h)
+		{
+			fprintf(stderr, "invalid/empty search string\n");
+			free(value); free(mask);
+			return 2;
 		}
 	}
 
-	if (!len || *h)
-	{
-		fprintf(stderr, "invalid/empty search string\n");
-		free(value); free(mask);
-		return 2;
-	}
-
-	if (argc < 3)
+	if (argc == 0)
 		searchfile("stdin", 0, value, mask, len);
 	else
 	{
-		int c = 2;
-		while (c < argc)
-			recurse(argv[c++], value, mask, len);
+		for (int i = 0; i < argc; i++)
+			recurse(*argv++, value, mask, len, &options);
 	}
 
 	free(value); free(mask);

test/bgrep-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly SCRIPT_PATH="$(dirname "$0")"
+
 PREPARE=1
 
 if [ $# -gt 0 ];then
@@ -37,10 +39,27 @@ fi
 # Test execution
 #
 echo Testing bgrep
-echo "../bgrep 424242 /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt"
+echo "${SCRIPT_PATH}/../bgrep 424242 /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt"
 
-../bgrep 424242 /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt
+"${SCRIPT_PATH}"/../bgrep 424242 /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt
 
-echo -e "\nDifferences between /tmp/results.txt and /tmp/bgrepExpectedResult.txt:"
+echo -e "\nDifferences between /tmp/results.txt and /tmp/bgrepExpectedResult.txt (arg):"
 
-diff -u /tmp/results.txt /tmp/bgrepExpectedResult.txt
+diff -u /tmp/results.txt /tmp/bgrepExpectedResult.txt || exit 1
+
+echo "424242" | xxd -p -r > /tmp/bgrep-pattern
+
+"${SCRIPT_PATH}"/../bgrep -f /tmp/bgrep-pattern /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt
+
+echo -e "\nDifferences between /tmp/results.txt and /tmp/bgrepExpectedResult.txt (file):"
+
+diff -u /tmp/results.txt /tmp/bgrepExpectedResult.txt || exit 1
+
+echo "424242" | xxd -p -r > /tmp/bgrep-pattern
+echo "FFFFFF" | xxd -p -r > /tmp/bgrep-mask
+
+"${SCRIPT_PATH}"/../bgrep -f /tmp/bgrep-pattern -m /tmp/bgrep-mask /tmp/bgrepTest* | sed 's/^.*: //g' > /tmp/results.txt
+
+echo -e "\nDifferences between /tmp/results.txt and /tmp/bgrepExpectedResult.txt (file+mask):"
+
+diff -u /tmp/results.txt /tmp/bgrepExpectedResult.txt || exit 1

commit 151e9247637e692210775121ae8000bc0f133f78

Fix wrong match offset
This happens when the search pattern size is bigger than half the
search buffer size. In this case, when `offset` is 0 (during the first
`while` iteration), `o` is set to `len`, which is bigger than `r`
(=`bufsize - len`), so the loop is bypassed and `offset` is then set to
`len` instead of the number of bytes read (=`r`).

The tests scripts cannot show that because the search pattern is always
too small (it needs to be at least 512 KB since the minimum buffer size
is 1 MB).

This bug was introduced with PR #21. I don't understand why this PR
changed that code.

bgrep.c
@@ -183,7 +183,7 @@ void searchfile(const char *filename, int fd, const unsigned char *value, const 
 			}
 		}
 
-		offset += o;
+		offset += r;
 	}
 
 	free(buf);